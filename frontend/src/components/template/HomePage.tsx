f69fc1f38047bf12511648d0778d0aa61625e25a
MrT-jesus-1995 <livemlg1995@gmail.com>
2022-11-12 23:55:06 +0700

    finish da home page

--- frontend/src/components/template/HomePage.tsx
+++ frontend/src/components/template/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Flex, Container, Heading, Stack, Text, Button, Icon, IconProps, Grid, GridItem, Box } from "@chakra-ui/react"
+import { Flex, Container, Heading, Stack, Text, Button, Icon, IconProps, Grid, GridItem, Box, Spacer } from "@chakra-ui/react"
 
 export default function CallToActionWithIllustration() {
     return (
@@ -11,32 +11,44 @@ export default function CallToActionWithIllustration() {
                     </Text>
                 </Heading>
                 <Text color={"gray.500"} maxW={"3xl"} fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}>
-                    วิชา เครือข่ายคอมพิวเตอร์เบื้องต้น
+                    <Text as={"span"} color={"#65BED1"}>
+                        วิชา{" "}
+                    </Text>
+                    เครือข่ายคอมพิวเตอร์เบื้องต้น
                 </Text>
             </Stack>
-            <Box bg="gray.300" w="100%" h="100%">
-                <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-                    <GridItem colSpan={2} bg="tomato">
-                        <Button colorScheme="blue" size="lg" align={"center"}>
-                            Button
+            <Box w="100%" h="100%">
+                <Grid templateColumns="repeat(5, 1fr)" gap={12} p={12} align={"center"}>
+                    <GridItem colSpan={2}>
+                        <Button bg="#B6BDDF" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                            บทที่ 1 พื้นฐานระบบเครือข่ายคอมพิวเตอร์{" "}
                         </Button>
                     </GridItem>
-                    <GridItem colStart={4} colEnd={6} bg="papayawhip">
-                        <Button colorScheme="blue" size="lg" align={"center"}>
-                            Button
+                    <GridItem colStart={4} colEnd={6}>
+                        <Button bg="#90B77D" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                            บทที่ 2 อุปกรณ์ระบบเครือข่าย
                         </Button>
                     </GridItem>
-                    <GridItem colSpan={2} bg="tomato">
-                        <Button colorScheme="blue" size="lg" align={"center"}>
-                            Button
+                    <GridItem colSpan={2}>
+                        <Button bg="#F96879" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                            บทที่ 3 ประเภทของระบบเครือข่าย
                         </Button>
                     </GridItem>
-                    <GridItem colStart={4} colEnd={6} bg="papayawhip">
-                        <Button colorScheme="blue" size="lg" align={"center"}>
-                            Button
+                    <GridItem colStart={4} colEnd={6}>
+                        <Button bg="#D887D0" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                            บทที่ 4 สื่อกลางนำสัญญาณในระบบเครือข่าย
                         </Button>
                     </GridItem>
                 </Grid>
+                <Flex gap={6} p={12}>
+                    <Spacer />
+                    <Box>
+                        <Button bg="#AAAAAA" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                            บทที่ 5 โปรโทคอล
+                        </Button>
+                    </Box>
+                    <Spacer />
+                </Flex>
             </Box>
         </Container>
     )
