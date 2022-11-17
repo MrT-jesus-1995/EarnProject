59e8714a714ab60de215063292fc536300b18090
earnpreeya11 <118009396+earnpreeya11@users.noreply.github.com>
2022-11-17 22:51:38 +0700

    Update index.tsx
    รอบ2แล้วเยดแม่มม

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,11 +1,13 @@
+import HomePage from "../../components/template/HomePage"
 import Header from "../../components/template/Header"
 import Footer from "../../components/template/Footer"
+import Chapter1 from "../../components/chapter/chapter1"
 
 const Home = () => {
     return (
         <>
             <Header />
-
+            <Chapter1 />
             <Footer />
         </>
     )
