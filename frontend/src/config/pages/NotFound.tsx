e100a080727eb0a02462bb71e643514a2709a01b
MrT-jesus-1995 <livemlg1995@gmail.com>
2022-11-18 19:47:57 +0700

    delet da useless

--- frontend/src/config/pages/NotFound.tsx
+++ frontend/src/config/pages/NotFound.tsx
@@ -1,20 +1,17 @@
 import { Box, Heading, VStack, Text } from "@chakra-ui/react"
-import AppBody from "../../components/share/app/AppBody"
 
 const NotFound = () => {
     return (
-        <AppBody>
-            <VStack maxW="100vw" minH="100vh" alignItems={"center"} justifyContent="center">
-                <Box p={5} bg="white" shadow={"lg"} rounded="xl">
-                    <Heading fontSize={{ base: "md", lg: "lg", xl: "2xl" }}>
-                        <Box as="span" color="red.400">
-                            404
-                        </Box>
-                        Page Not Found
-                    </Heading>
-                </Box>
-            </VStack>
-        </AppBody>
+        <VStack maxW="100vw" minH="100vh" alignItems={"center"} justifyContent="center">
+            <Box p={5} bg="white" shadow={"lg"} rounded="xl">
+                <Heading fontSize={{ base: "md", lg: "lg", xl: "2xl" }}>
+                    <Box as="span" color="red.400">
+                        404
+                    </Box>
+                    Page Not Found
+                </Heading>
+            </Box>
+        </VStack>
     )
 }
 
