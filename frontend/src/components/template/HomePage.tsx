99a659ca653f51c940ee2a1a4893069aa62b20e5
earnpreeya11 <118009396+earnpreeya11@users.noreply.github.com>
2022-12-10 13:39:02 +0700

    9/12/65
    เพิ่มเนื้อหาบท1 เมื่อวานเอง ชิลๆ

--- frontend/src/components/template/HomePage.tsx
+++ frontend/src/components/template/HomePage.tsx
@@ -5,12 +5,12 @@ export default function CallToActionWithIllustration() {
         <Container maxW={"9xl"}>
             <Stack textAlign={"center"} align={"center"} spacing={{ base: 8, md: 10 }} py={{ base: 20, md: 28 }}>
                 <Heading fontWeight={600} fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }} lineHeight={"110%"}>
-                    เว็บไซต์บทเรียนการ
+                    เว็บไซต์ทบทวนบทเรียน
                     <Text as={"span"} color={"#42D2C3"}>
-                        สอนออนไลน์
+                        ออนไลน์
                     </Text>
                 </Heading>
-                <Text color={"gray.500"} maxW={"3xl"} fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}>
+                <Text color={"gray.500"} maxW={"3xl"} fontSize={{ base: "xl", sm: "1xl", md: "5xl" }}>
                     <Text as={"span"} color={"#65BED1"}>
                         วิชา{" "}
                     </Text>
@@ -20,22 +20,22 @@ export default function CallToActionWithIllustration() {
             <Box w="100%" h="100%">
                 <Grid templateColumns="repeat(5, 1fr)" gap={12} p={12} align={"center"}>
                     <GridItem colSpan={2}>
-                        <Button bg="#B6BDDF" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                        <Button bg="#B1E1FF" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
                             บทที่ 1 พื้นฐานระบบเครือข่ายคอมพิวเตอร์
                         </Button>
                     </GridItem>
                     <GridItem colStart={4} colEnd={6}>
-                        <Button bg="#90B77D" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                        <Button bg="#C8FFD4" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
                             บทที่ 2 อุปกรณ์ระบบเครือข่าย
                         </Button>
                     </GridItem>
                     <GridItem colSpan={2}>
-                        <Button bg="#F96879" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                        <Button bg="#FFE15D" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
                             บทที่ 3 ประเภทของระบบเครือข่าย
                         </Button>
                     </GridItem>
                     <GridItem colStart={4} colEnd={6}>
-                        <Button bg="#D887D0" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                        <Button bg="#FFABE1" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
                             บทที่ 4 สื่อกลางนำสัญญาณในระบบเครือข่าย
                         </Button>
                     </GridItem>
@@ -43,7 +43,7 @@ export default function CallToActionWithIllustration() {
                 <Flex gap={6} p={12}>
                     <Spacer />
                     <Box>
-                        <Button bg="#AAAAAA" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
+                        <Button bg="#ECC5FB" size="lg" fontSize={"xl"} style={{ width: "500px", height: "75px" }} borderRadius="0" border="2px">
                             บทที่ 5 โปรโทคอล
                         </Button>
                     </Box>
