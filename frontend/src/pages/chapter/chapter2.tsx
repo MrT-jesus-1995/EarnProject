a59f745573c527a922434ea9c6d418e5bc8710a8
earnpreeya11 <118009396+earnpreeya11@users.noreply.github.com>
2022-12-10 22:00:01 +0700

    10/12/65  22:00น.
    ลงเนื้อหาบท 1 และบท2 เสร็จแล้ว **เหลือแค่ลงรูปของเนื้อหาทั้งบท1 และบท2

---
+++ frontend/src/pages/chapter/chapter2.tsx
@@ -0,0 +1,17 @@
+import HomePage from "../../components/template/HomePage"
+import Header from "../../components/template/Header"
+import Footer from "../../components/template/Footer"
+import Chapter1 from "../../components/chapter/chapter1"
+import Chapter2 from "../../components/chapter/chapter2"
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Chapter2 />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
